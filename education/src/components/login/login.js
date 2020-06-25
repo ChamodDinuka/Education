d5799e748442c4b80d923715e335f5c423a7c531
2017cs030 <chamoddinuka518@gmail.com>
2020-06-25 08:00:02 +0530

    set backend

--- education/src/components/login/login.js
+++ education/src/components/login/login.js
@@ -2,13 +2,39 @@ import React,{Component} from 'react'
 import './login.css'
 
 class Login extends Component{
+  state={
+    email:'',
+    password:''
+  }
+  changeEmail=(e)=>{
+    this.setState({
+      email:e.target.value
+
+    });
+  }
+  changePassword=(e)=>{
+    this.setState({
+      password:e.target.value
+
+    });
+  }
+  submit=(e)=>{
+    e.preventDefault();
+    console.log(this.state.email);
+    console.log(this.state.password);
+    var key=(this.state.email=='admin@gmail.com')?"True":"False"
+    console.log(key)
+
+  }
     render(){
         return(
-            <div className="login-form">
+            <div className="login-form" >
+              <form onSubmit={this.submit}>
               <h1>Login</h1>
-              <label id="email">Email</label><input type="email" id="email" name="email"/><br/>
-              <label>Password </label><input type="password"id="password" name="password"/> <br/><br/>
+              <label id="email">Email</label><input type="email" id="email" name="email" onChange={this.changeEmail}/><br/>
+              <label>Password </label><input type="password"id="password" name="password" onChange={this.changePassword}/> <br/><br/>
               <button type="submit" id="sumbmit" name="submit">Login</button>
+              </form>
             </div>
         );
         }
